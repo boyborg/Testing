8b1271949e4fc2c9420e1911f6da31d6970b2713
Rosyid <rosyid593@gmail.com>
2019-09-23 13:43:56 +0700

    first

---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test{
+   
+
+    public static void main(String[] args){
+        String nama="raka";
+        int umur=24;
+
+        if(umur>20){
+        System.out.println(nama+" syarat terpenuhi");
+        }
+        else{
+        System.out.println(nama+" syarat tidak terpenuhi");
+        }
+
+    }
+
+
+}
